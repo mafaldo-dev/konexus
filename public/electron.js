f6e312a6e87ddc25411061e5785ccb73ba7249f2
mafaldo-dev <03.09gui.mafaldo@gmail.com>
2025-07-19 16:00:00 -0300

    fix: correção no caminho de iniciação

--- public/electron.js
+++ public/electron.js
@@ -17,7 +17,7 @@ function createWindow() {
   win.loadURL(
     isDev
       ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+      : `file://${path.join(__dirname, 'build', 'index.html')}`
   );
 
   if (isDev) {
