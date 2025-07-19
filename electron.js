dd812b5d1894b8d3a8d018cb5a40cb8f8b3add89
mafaldo-dev <03.09gui.mafaldo@gmail.com>
2025-07-19 17:21:54 -0300

    fix: correção no caminho

--- electron.js
+++ electron.js
@@ -1,41 +1,33 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
-    height:935,
+    height: 935,
     width: 1300,
-    titleBarStyle:'default',
+    titleBarStyle: 'default',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
 
-  win.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, 'build', 'index.html')}`
-  );
-
   if (isDev) {
+    win.loadURL('http://localhost:3000');
     win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, 'build', 'index.html'));
   }
-  win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
